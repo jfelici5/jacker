638c3397964f50cf7dd12bd6a64c164ae034d521
jfelici5 <jfelici5@u.rochester.edu>
2020-07-03 15:42:02 -0700

    changed styling

--- jacker/src/Form.js
+++ jacker/src/Form.js
@@ -1,6 +1,9 @@
-import React, { Component } from 'react'
+import ReactDOM from 'react-dom';
+import React, { Component } from 'react';
+import {Modal, Button} from 'antd';
 
-class Form extends Component{
+
+class Form extends React.Component{
     constructor(props) {
         super(props)
 
@@ -9,6 +12,7 @@ class Form extends Component{
             desiredGPA: 0
         }
     }
+
 
     handleGPAChange = (event) => {
         this.setState({
@@ -22,31 +26,74 @@ class Form extends Component{
         })
     }
 
+    handleSubmit = (event) => {
+        console.log(this.state.currentGPA)
+    }
+
+    state = { visible: false };
+
+    showModal = () => {
+      this.setState({
+        visible: true,
+      });
+    };
+  
+    handleOk = e => {
+      console.log(e);
+      this.setState({
+        visible: false,
+      });
+    };
+  
+    handleCancel = e => {
+      console.log(e);
+      this.setState({
+        visible: false,
+      });
+    };
+
     render(){
         return (
-            <form>
-                <div>
-                    <label>
-                        Current GPA:
-                    </label>
-                    <input type = 'number' 
-                    step = "0.01" 
-                    value = {this.state.currentGPA}
-                    onChange = {this.handleGPAChange} />
-                </div>
-                <div>
-                    <label>
-                        Desired GPA:
-                    </label>
-                    <input type = 'number'
-                    step = '0.01'
-                    value = {this.state.desiredGPA}
-                    onChange = {this.handleDesiredChange}
-                    />
-                </div>
+            <div>
+            <form id = "myform">
+                <label>
+                    Current GPA:                    
+                </label>
+                <input type = 'number' 
+                step = "0.01" 
+                value = {this.state.currentGPA}
+                onChange = {this.handleGPAChange} />
+                <label>
+                    Desired GPA:                    
+                </label>
+                <input type = 'number'
+                step = '0.01'
+                value = {this.state.desiredGPA}
+                onChange = {this.handleDesiredChange}
+                />
+                <Button type = "button" onClick={this.showModal}>
+                    Submit
+                </Button>
+                <Modal
+                    title="Basic Modal"
+                    visible={this.state.visible}
+                    onOk={this.handleOk}
+                    onCancel={this.handleCancel}
+                    >
+                    <p>Some contents...</p>
+                    <p>Some contents...</p>
+                    <p>Some contents...</p>
+                </Modal>
+
+                
             </form>
+            
+            </div>
         )
     }
 }
+
+
+
 
 export default Form
