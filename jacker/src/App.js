4a8f9e589de354a0b30935c43054ee7826fdbbfe
jfelici5 <jfelici5@u.rochester.edu>
2020-07-03 15:42:34 -0700

    fixed routing

--- jacker/src/App.js
+++ jacker/src/App.js
@@ -1,12 +1,15 @@
 import React from 'react';
 import logo from './logo.svg';
 import './App.css';
-import Form from'./Form.js'
+import Form from'./Form.js';
+import {Button} from 'antd';
 
-function App() {
-  return (
-    <Form/>
-  );
+function App(){
+  return(
+    <Button>
+      hey
+    </Button>
+  )
 }
 
-export default App;
+export default App
--- jacker/src/index.js
+++ jacker/src/index.js
@@ -1,12 +1,13 @@
 import React from 'react';
 import ReactDOM from 'react-dom';
 import './index.css';
-import App from './App';
 import * as serviceWorker from './serviceWorker';
+import Form from './Form'
+import App from './App'
 
 ReactDOM.render(
   <React.StrictMode>
-    <App />
+    <Form />
   </React.StrictMode>,
   document.getElementById('root')
 );
