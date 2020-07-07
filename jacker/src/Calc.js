dca451c5db68cfe33cf75e259a05921c963ba2f3
jfelici5 <jfelici5@u.rochester.edu>
2020-07-06 17:59:44 -0700

    changed name and formatting of form

---
+++ jacker/src/Calc.js
@@ -0,0 +1,150 @@
+import ReactDOM from 'react-dom';
+import React, { Component } from 'react';
+import {Modal, Button, Input, InputNumber, Form} from 'antd';
+
+//desiredpoints = (desiredGPA*totalCredits)-(currentGPA*completedCredits)
+//thus, 
+//desiredPoints = (4*16)-(4*12)
+
+const layout = {
+    labelCol: { span: 8 },
+    wrapperCol: { span: 16 },
+  };
+  const tailLayout = {
+    wrapperCol: { offset: 8, span: 16 },
+  };
+
+class Calc extends React.Component{
+    constructor(props) {
+        super(props)
+
+        this.state = {
+            completedUnits: 0,
+            remainingUnits: 0,
+            currentGPA: 0,
+            desiredGPA: 0,
+            neededGPA: 0,
+            desiredPoints: ((parseFloat(this.state.desiredGPA)*(parseFloat(this.state.completedUnits)+parseFloat(this.state.remainingUnits))) - (parseFloat(this.state.currentGPA)*parseFloat(this.state.completedUnits))),
+        }
+    }
+
+    handleGPAChange = (value) => {
+        this.setState({
+            currentGPA: value,
+        })
+    }
+
+    handleCompletedUnitsChange = (value) => {
+        this.setState({
+            completedUnits: value,
+        })
+    }
+    
+    handleRemainingUnitsChange = (value) => {
+        this.setState({
+            remainingUnits: value,
+        })
+    }
+
+    handleDesiredChange = (value) => {
+        this.setState({
+            desiredGPA: value,
+
+        })
+    }
+
+    state = { visible: false };
+
+    showModal = () => {
+      this.setState({
+        visible: true,
+        coolGPA: ((parseFloat(this.state.desiredGPA)*(parseFloat(this.state.completedUnits)+parseFloat(this.state.remainingUnits))) - (parseFloat(this.state.currentGPA)*parseFloat(this.state.completedUnits)))/this.state.remainingUnits
+ 
+    });
+
+    };
+  
+    handleOk = e => {
+      console.log(e);
+      this.setState({
+        visible: false,
+      });
+    };
+  
+    handleCancel = e => {
+      console.log(e);
+      this.setState({
+        visible: false,
+      });
+    };
+
+    onFinish = values => {
+        console.log('Success:', values);
+      };
+    
+    onFinishFailed = errorInfo => {
+        console.log('Failed:', errorInfo);
+      };
+
+    render(){
+        return (
+            <div>
+            <Form
+            {...layout}
+            name ="basic" 
+            >
+            <Form.Item
+            label = "Completed units"
+            name = "completedUnits"
+            >
+                <InputNumber 
+                onChange = {this.handleCompletedUnitsChange}/>
+            </Form.Item>
+            <Form.Item
+            label = "Remaining units"
+            name = "remainingUnits"
+            >
+                <InputNumber 
+                onChange = {this.handleRemainingUnitsChange}/>
+            </Form.Item>
+            <Form.Item
+            label = "Current GPA"
+            name = "currentGPA"
+            >
+                <InputNumber 
+                onChange = {this.handleGPAChange}/>
+            </Form.Item>
+            <Form.Item
+            label = "Desired GPA"
+            name = "desiredGPA"
+            >
+                <InputNumber 
+                onChange = {this.handleDesiredChange}/>
+            </Form.Item>
+            
+            
+                <Button type = "button" onClick={this.showModal}>
+                    Submit
+                </Button>
+                <Modal
+                    title="Basic Modal"
+                    visible={this.state.visible}
+                    onOk={this.handleOk}
+                    onCancel={this.handleCancel}
+                    coolGPA = {this.state.coolGPA}                    
+
+                >
+                    <p> You need a minimum GPA of {this.state.coolGPA} across your remaining courses to achieve a total GPA of {this.state.desiredGPA}</p>
+                    <p>Some contents...</p>
+                    <p>Some contents...</p>
+                </Modal>
+
+                
+            </Form>
+            
+            </div>
+        )
+    }
+}
+
+export default Calc
